42f39ece3dd75a902a6d74ce54feaa2fca96a410
Anthony Helka <helkaanthony1997@gmail.com>
2020-04-25 18:39:09 -0400

    Added react-router-dom. Added Login, Register and Feed pages. Added Protected Routes.

---
+++ src/components/Feed/Feed.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Feed.css';
+
+const Feed = () => {
+  return (
+    <div>
+      Feed
+    </div>
+  );
+};
+
+export default Feed;
---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const fakeAuth = {
+  isAuthenticated: false
+}
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route {...rest} render={(props) => (
+      fakeAuth.isAuthenticated === true
+        ? <Component {...props} />
+        : <Redirect to='/login' />
+    )} />
+  );
+};
+
+export default PrivateRoute;
