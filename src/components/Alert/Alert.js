16765a1f5233d5dbd781f30a0a4946cc209b38ce
Jon Evans <glitch100@gmail.com>
2020-05-02 01:27:46 +0100

    refactored alerts

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -7,37 +7,40 @@ import { removeAlert } from '../redux/modules/alert';
 import styles from './Alert.module.css';
 import dismiss from '../../images/icons/x.svg';
 
-const Alert = ({ c, alerts, remove }) => (
+const Alert = ({ alerts, remove }) => (
   <div className={styles.alertsContainer}>
     {alerts !== null &&
-      alerts.length > 0 &&
-      alerts.map((alert) => (
-        <div
-          className={[styles.alertContainer, styles[alert.alertType]].join(' ')}
-          key={alert.id}
-        >
-          {alert.message}
-          <img
-            className={styles.cancelButton}
-            src={dismiss}
-            alt="Dismiss Alert"
-            onClick={() => remove(alert.id)}
-          />
-        </div>
-      ))}
+      Object.keys(alerts).length > 0 &&
+      Object.keys(alerts).map((alertId) => {
+        return (
+          <div
+            className={[
+              styles.alertContainer,
+              styles[alerts[alertId].alertType],
+            ].join(' ')}
+            key={alertId}
+          >
+            {alerts[alertId].message}
+            <img
+              className={styles.cancelButton}
+              src={dismiss}
+              alt="Dismiss Alert"
+              onClick={() => remove(alertId)}
+            />
+          </div>
+        );
+      })}
   </div>
 );
 
 Alert.propTypes = {
-  c: PropTypes.number,
   alerts: PropTypes.array,
   remove: PropTypes.func,
 };
 
 const mapStateToProps = (state) => {
   return {
-    c: Math.random(),
-    alerts: state.alert.alerts,
+    alerts: state.alert,
   };
 };
 
--- src/components/redux/modules/alert.js
+++ src/components/redux/modules/alert.js
@@ -1,9 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 import produce from 'immer';
 
-const initialState = {
-  alerts: [],
-};
+const initialState = {};
 
 const SET_ALERT = 'SET_ALERT';
 const setAlert = (payload) => ({
@@ -21,14 +19,11 @@ const alert = (state = initialState, action) =>
   produce(state, (draft) => {
     switch (action.type) {
       case SET_ALERT:
-        draft.alerts.push(action.payload);
+        draft[action.payload.id] = action.payload;
         return draft;
       case REMOVE_ALERT:
-        if (state.alerts.some((item) => item.id === action.id)) {
-          draft.alerts.splice(
-            draft.alerts.findIndex((item) => item.id === action.id),
-            1
-          );
+        if (state[action.id]) {
+          delete draft[action.id];
           return draft;
         }
         return state;
