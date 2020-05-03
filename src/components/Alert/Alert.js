e81512cbc293bce8acfd7b0b0cfe9ec9cf372d28
Anthony Helka <helkaanthony1997@gmail.com>
2020-05-03 17:46:23 -0400

    Refactored the Alert.js component to use Object.Values instead of Object.Keys.

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -7,32 +7,32 @@ import { removeAlert } from '../redux/modules/alert';
 import styles from './Alert.module.css';
 import dismiss from '../../images/icons/x.svg';
 
-const Alert = ({ alerts, remove }) => (
-  <div className={styles.alertsContainer}>
-    {alerts !== null &&
-      Object.keys(alerts).length > 0 &&
-      Object.keys(alerts).map((alertId) => {
-        return (
-          <div
-            className={[
-              styles.alertContainer,
-              styles[alerts[alertId].alertType],
-            ].join(' ')}
-            key={alertId}
-          >
-            {alerts[alertId].message}
-            <img
-              className={styles.cancelButton}
-              src={dismiss}
-              alt="Dismiss Alert"
-              onClick={() => remove(alertId)}
-            />
-          </div>
-        );
-      })}
-  </div>
-);
-
+const Alert = ({ alerts, remove }) => {
+  return (
+    <div className={styles.alertsContainer}>
+      {alerts !== null &&
+        Object.values(alerts).length > 0 &&
+        Object.values(alerts).map((alert) => {
+          return (
+            <div
+              className={[styles.alertContainer, styles[alert.alertType]].join(
+                ' '
+              )}
+              key={alert.id}
+            >
+              {alert.message}
+              <img
+                className={styles.cancelButton}
+                src={dismiss}
+                alt="Dismiss Alert"
+                onClick={() => remove(alert.id)}
+              />
+            </div>
+          );
+        })}
+    </div>
+  );
+};
 Alert.propTypes = {
   alerts: PropTypes.object,
   remove: PropTypes.func,
