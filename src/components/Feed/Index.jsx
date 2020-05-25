3ace863efbc2cc072b0661e028084f4dacf8dcfa
Anthony Helka <helkaanthony1997@gmail.com>
2020-05-24 20:16:14 -0400

    Added RightModal component. Fixed styling for Feed component.

--- src/components/Feed/Index.jsx
+++ src/components/Feed/Index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import FeedCard from '../FeedCard/Index';
+import RightModal from '../RightModal/Index';
 
 import styles from './Feed.module.css';
 
@@ -8,35 +9,38 @@ import { feedData } from '../utils/dummyData';
 
 const Feed = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.feedNavBar}>
-        <div className={[styles.feedNavBarItem, styles.active].join(' ')}>
-          <span>Discover</span>
-          <div className={styles.dot} />
-        </div>
-        <div className={[styles.feedNavBarItem, styles.inactive].join(' ')}>
-          <span>Matchmaking</span>
-        </div>
-      </div>
-      <div className={styles.titleContainer}>
-        <span className={styles.title}>Live Streams</span>
-      </div>
+    <div className={styles.pageContainer}>
       <div className={styles.feedContainer}>
-        {feedData !== null &&
-          feedData.map((feedItem, index) => (
-            <FeedCard
-              key={`${index}-${feedItem.id}`}
-              id={feedItem.id}
-              userName={feedItem.user.name}
-              userAvatar={feedItem.user.icon}
-              title={feedItem.title}
-              subTitle={feedItem.subTitle}
-              sourceImg={feedItem.sourceImg}
-              viewerCount={feedItem.viewerCount}
-              isLive={feedItem.isLive}
-            />
-          ))}
+        <div className={styles.feedNavBar}>
+          <div className={[styles.feedNavBarItem, styles.active].join(' ')}>
+            <span>Discover</span>
+            <div className={styles.dot} />
+          </div>
+          <div className={[styles.feedNavBarItem, styles.inactive].join(' ')}>
+            <span>Matchmaking</span>
+          </div>
+        </div>
+        <div className={styles.titleContainer}>
+          <span className={styles.title}>Live Streams</span>
+        </div>
+        <div className={styles.discoverContainer}>
+          {feedData !== null &&
+            feedData.map((feedItem, index) => (
+              <FeedCard
+                key={`${index}-${feedItem.id}`}
+                id={feedItem.id}
+                userName={feedItem.user.name}
+                userAvatar={feedItem.user.icon}
+                title={feedItem.title}
+                subTitle={feedItem.subTitle}
+                sourceImg={feedItem.sourceImg}
+                viewerCount={feedItem.viewerCount}
+                isLive={feedItem.isLive}
+              />
+            ))}
+        </div>
       </div>
+      <RightModal />
     </div>
   );
 };
---
+++ src/components/RightModal/Index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './RightModal.module.css';
+
+const RightModal = () => {
+  return <div className={styles.container}></div>;
+};
+
+export default RightModal;
