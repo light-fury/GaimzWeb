b5381da222efc6fec5f7c9f6fc7e9376f559eaee
Conlat <conradjonp@gmail.com>
2020-06-07 19:41:58 +0800

    some fix on styles crashing

--- src/components/MatchmakingSettings/index.tsx
+++ src/components/MatchmakingSettings/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import styles from './MatchmakingSettings.module.css';
 
 interface MatchmakingSettingsProps {
 
 }
 
-const MatchmakingSettings = ({ }: MatchmakingSettingsProps) => (
-  <div />
-);
+const MatchmakingSettings = ({ }: MatchmakingSettingsProps) => {
+
+  return (
+    <div>
+      <div className={styles.title}>
+        SEARCH SETTINGS
+      </div>
+    </div>
+  );
+};
 
 export default MatchmakingSettings;
